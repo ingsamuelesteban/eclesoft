31f9761f4c10370f7f9793e0e55e9992bba218ca
ingsamuelesteban <ing.samuelesteban@gmail.com>
2023-05-04 17:00:00 -0400

    tam inp name

--- resources/views/livewire/crear-acta.blade.php
+++ resources/views/livewire/crear-acta.blade.php
@@ -107,7 +107,7 @@
         <x-text-input id="nombre" class="block mt-1 w-full" type="text" 
         wire:model="nombre" 
         :value="old('nombre')" placeholder="Primer y Segundo Nombre"
-        maxlength="20"/>
+        maxlength="35"/>
 
         @error('nombre')
             <livewire:mostrar-alertas :message="$message" />
