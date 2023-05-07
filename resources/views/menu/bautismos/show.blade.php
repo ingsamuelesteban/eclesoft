ca7eb00352074faac2a13aa9af6e773fcbe7856b
ingsamuelesteban <ing.samuelesteban@gmail.com>
2023-05-07 12:52:08 -0400

    prueba nueva forma de abrir acta

--- resources/views/menu/bautismos/show.blade.php
+++ resources/views/menu/bautismos/show.blade.php
@@ -2,21 +2,34 @@
     @include('menu.bautismos.navigation');
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Editar Acta de Bautismo') }}
+            {{ $bautismo->nombre}}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                  <h1 class="text-2xl font-bold text-center my-10">
-                    Editar Acta: {{ $bautismo->nombre}}
-                  </h1>
-                    <div class="md:flex md:justify-center p-5">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg " >
+                <div class="p-10">
+                    <div class="mb-5">
+                        <h3 class="font-bold text-3xl text-gray-800 my-3">
+                            {{ $bautismo->nombre}}
+                        </h3>
 
+                        <div>
+                            <h3 class="font-bold text-3xl text-gray-800 my-3 text-center">Datos del Libro de Bautismo</h3>
+                            <div class="grid grid-cols-3">
+                                <p class="font-bold text-sm uppercase text-gray-800 my-3">Libro:
+                                    <span class="normal-case font-normal">{{ $bautismo->libro_bautismo}}</span>
+                                </p>
+                                <p class="font-bold text-sm uppercase text-gray-800 my-3">Folio:
+                                    <span class="normal-case font-normal">{{ $bautismo->folio_bautismo}}</span>
+                                </p>
+                                <p class="font-bold text-sm uppercase text-gray-800 my-3">Acta
+                                    <span class="normal-case font-normal">{{ $bautismo->no_bautismo}</span>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
