4199ceeb303682666ae2a8a1deeb62f928ce6277
ingsamuelesteban <ing.samuelesteban@gmail.com>
2023-07-06 11:32:19 -0400

    agregar circunscripcion a la impresion

--- resources/views/livewire/mostrar-parroquia.blade.php
+++ resources/views/livewire/mostrar-parroquia.blade.php
@@ -15,6 +15,7 @@
                 <p class="text-sm text-gray-600 font-bold">Calle: {{ $parroquia->calle}}</p>
                 <p class="text-sm text-gray-600 font-bold">Ciudad: {{ $parroquia->ciudad}}</p>
                 <p class="text-sm text-gray-600 font-bold">Provincia: {{ $parroquia->provincia}}</p>
+                <p class="text-sm text-gray-600 font-bold">Provincia: {{ $parroquia->circunscripcion}}</p>
             </div> 
 
             <div class="flex flex-col md:flex-row items-stretch gap-3  mt-5 md:mt-0">
