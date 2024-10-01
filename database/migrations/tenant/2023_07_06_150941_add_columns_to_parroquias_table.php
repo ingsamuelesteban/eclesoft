b8bfcdc0ef91ee25f8f3d2573443f954be8fe7aa
ingsamuelesteban <110210780+ingsamuelesteban@users.noreply.github.com>
2024-10-01 16:57:19 -0400

    Update 2023_07_06_150941_add_columns_to_parroquias_table.php

--- database/migrations/tenant/2023_07_06_150941_add_columns_to_parroquias_table.php
+++ database/migrations/tenant/2023_07_06_150941_add_columns_to_parroquias_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::table('parroquias', function (Blueprint $table) {
             $table->string('circunscripcion');
+             $table->string('color_borde');
         });
     }
 
