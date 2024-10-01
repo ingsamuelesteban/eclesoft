c4ffc5723455c093c908f844876f9b87075c1f3f
ingsamuelesteban <110210780+ingsamuelesteban@users.noreply.github.com>
2024-10-01 17:09:15 -0400

    Update 2024_06_22_144155_add_columns_to_decretos_table.php

--- database/migrations/tenant/2024_06_22_144155_add_columns_to_decretos_table.php
+++ database/migrations/tenant/2024_06_22_144155_add_columns_to_decretos_table.php
@@ -12,6 +12,27 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('decretos', function (Blueprint $table) {
+            $table->string('bautismo_id')->nullable();
+            $table->string('nombre')->nullable();
+            $table->string('genero')->nullable();
+            $table->date('fecha_nacimiento')->nullable();
+            $table->string('lugar_nacimiento')->nullable();
+            $table->string('nombre_madre')->nullable();
+            $table->string('cedula_madre')->nullable();
+            $table->string('nombre_padre')->nullable();
+            $table->string('cedula_padre')->nullable();
+            $table->string('nombre_madrina')->nullable();
+            $table->string('nombre_padrino')->nullable();
+            $table->string('nombre_civil')->nullable();
+            $table->string('genero_civil')->nullable();
+            $table->date('fecha_nacimiento_civil')->nullable();
+            $table->string('lugar_nacimiento_civil')->nullable();
+            $table->string('nombre_madre_civil')->nullable();
+            $table->string('cedula_madre_civil')->nullable();
+            $table->string('nombre_padre_civil')->nullable();
+            $table->string('cedula_padre_civil')->nullable();
+            $table->string('nombre_madrina_civil')->nullable();
+            $table->string('nombre_padrino_civil')->nullable();
             $table->string('libro_nacimiento')->nullable();
             $table->string('folio_nacimiento')->nullable();
             $table->string('acta_nacimiento')->nullable();
